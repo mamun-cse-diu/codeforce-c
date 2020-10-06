6915309890d94fec017ad09c14e390762bd90f1c
MD.AL-MAMUN <59719431+mamun-cse-diu@users.noreply.github.com>
2020-10-06 10:31:18 +0600

    Add files via upload

---
+++ Fence.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int long long a,b,c;
+        cin>>a>>b>>c;
+        cout<< a+b+c-1 <<endl;
+    }
+}
